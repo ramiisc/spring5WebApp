65bc68ee4fa09b03f34b9183727b636c0802e7b9
Ramakrishna Potluru <prk.iisc@gmail.com>
2018-09-26 15:49:04 +0530

    First commit

---
+++ src/main/java/com/rpotluru/spring/spring5WebApp/Spring5WebAppApplication.java
@@ -0,0 +1,12 @@
+package com.rpotluru.spring.spring5WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebAppApplication.class, args);
+	}
+}
